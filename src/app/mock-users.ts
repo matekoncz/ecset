facf50860aa16a47d1e0df63f33300e982e167e9
matekoncz <matekoncz@users.noreply.github.com>
2023-12-03 22:55:23 +0000

    deploy: b964646c7727cf1819a4f1140557cf8b0d424524

--- src/app/mock-users.ts
+++
@@ -1,4 +0,0 @@
-import { User } from "./user";
-export const registered_users: User[] = [
-    {id: 0, firstname: 'admin', lastname: 'admin', username: 'admin', password: 'admin',profilepicurl: "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"}
-]
--- src/app/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User{
-    id: Number,
-    username: String,
-    firstname: String,
-    lastname: String,
-    password: String,
-    profilepicurl: String
-}
